f29cac958eb9b306c39379bd41c6488a9d0397c2
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-02-03 20:19:52 +0300

    Стабильная версия 0.1

--- NumberToThePower.cpp
+++ NumberToThePower.cpp
@@ -1,9 +1,37 @@
-﻿// NumberToThePower.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿// Напишите программу, которая запрашивает два целых числа
+// x и y, после чего вычисляет и выводит значение x в степени y.
 
 #include <iostream>
+#include <windows.h>
+using namespace std;
+
+/// <summary>
+/// Метод возведения X в степень Y
+/// </summary>
+/// <param name="x"></param>
+/// <param name="y"></param>
+/// <returns>int X возведённое в степень Y</returns>
+int CalcPower(int x, int y)
+{
+    int result = 1;
+
+    for (int i = 0; i < y; i++)
+        result *= x;
+
+    return result;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int X, Y;
+
+    cout << "Чтобы возвести X в степень Y введите X и Y : ";
+    cin >> X >> Y;
+
+    int result =   CalcPower(X, Y);
+
+    cout << X << " в степени " << Y << " = " << result;
 }
